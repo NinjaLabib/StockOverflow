6d14841bf3f287b2e649ad51b10a8db6e9114d3e
Ihtesham Chowdhury <NinjaLabib@gmail.com>
2017-01-29 02:15:41 -0500

    Changing stuff for D3 again in server

--- getStocks.js
+++ getStocks.js
@@ -4,7 +4,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var googleFinance = require('google-finance');
 var _ = require('lodash');
-var app     = express();
+var times = require('async/times');
+ app     = express();
 var port_number =(process.env.PORT || 8081);
 app.all('*', function(req, res, next) {
      var origin = req.get('origin');
@@ -115,67 +116,53 @@ app.get('/getBounds', function(req, res){
         // y = _.concat(y,close);
 
       }
-
-
       res.send({
+        quotes:quotes,
         max: max,
         min:min
       });
+
+      // res.send({
+      //   max: max,
+      //   min:min
+      // });
 
   });
 
 })
 
 // app.get('/getNews', function(req, res){
-//
-//   fromR = req.query.from;
-//   toR = req.query.to;
-//   tickerR = req.query.;
-//   max = -1;
-//   min = -1;
-//   googleFinance.historical({
-//     symbol:tickerR,//+req.ticker
-//     from: fromR, //req.from
-//     to: toR //req.to
-//   }, function (err, quotes) {
-//       console.log(""+quotes[2].date);
-//       months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
-//       'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
-//       'Nov':11, 'Dec':12};
-//       x = [];
-//       y = [];
-//       max = -1;
-//       min = Number.MAX_SAFE_INTEGER;
-//       for(i = 0; i < quotes.length; i++){
-//         dateUnparsed = ""+quotes[i].date;
-//         dateSplit = dateUnparsed.split(" ");
-//         dateParsed = "" + _.get(months, dateSplit[1]) + "-"
-//           + dateSplit[2] + "-" + dateSplit[3];
-//         close = quotes[i].close;
-//         if (close > max) {
-//           max = close;
-//         }
-//         if (close < min) {
-//           min = close;
-//         }
-//         refined = {
-//           x: dateParsed,
-//           y: close
-//         };
-//         quotes[i] = refined;
-//
-//         // x = _.concat(x,dateParsed);
-//         // y = _.concat(y,close);
-//
-//       }
+//   var filterNews = function(start, final, q, callback) {
+//     startYear = start.substring(0,4);
+//     startMonth = start.substring(4,6);
+//     endYear = start.substring(0,4);
+//     endMonth = start.substring(4,6);
 //
 //
-//       res.send({
-//         max: max,
-//         min:min
-//       });
+//     callback(null,{
+//       url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+//       qs: {
+//         'api-key': "b572113326ca44698852b86b199fe673",
+//         'q': q,
+//         'begin_date': begin,
+//         'end_date': end
+//     })
+//   }
+//   request.get({
+//   url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+//   qs: {
+//     'api-key': "b572113326ca44698852b86b199fe673",
+//     'q': "GOOGLE",
+//     'begin_date': "20140223",
+//     'end_date': "20160312"
+//   },
+// }, function(err, response, body) {
+//   body = JSON.parse(body);
+//   res.send(body);
+// })
 //
-//   });
+//
+//
 //
 // })
 
