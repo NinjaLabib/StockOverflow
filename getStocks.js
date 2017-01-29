e051ba9fbfd56192ca806731ad06bbd0e5ea092d
Ihtesham Chowdhury <NinjaLabib@gmail.com>
2017-01-29 00:23:12 -0500

    Added getBounds for max and min

--- getStocks.js
+++ getStocks.js
@@ -60,9 +60,64 @@ app.get('/getData', function(req, res){
 
       }
 
+      res.send(quotes);
+      // res.send({
+      //   quotePairs:quotes,
+      //   max: max,
+      //   min:min
+      // });
+
+  });
+
+})
+app.get('/getBounds', function(req, res){
+  console.log(req.query.from);
+  console.log(req.query.to);
+  console.log(req.query.ticker);
+
+  fromR = req.query.from;
+  toR = req.query.to;
+  tickerR = req.query.ticker;
+  max = -1;
+  min = -1;
+  googleFinance.historical({
+    symbol:tickerR,//+req.ticker
+    from: fromR, //req.from
+    to: toR //req.to
+  }, function (err, quotes) {
+      console.log(""+quotes[2].date);
+      months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
+      'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
+      'Nov':11, 'Dec':12};
+      x = [];
+      y = [];
+      max = -1;
+      min = Number.MAX_SAFE_INTEGER;
+      for(i = 0; i < quotes.length; i++){
+        dateUnparsed = ""+quotes[i].date;
+        dateSplit = dateUnparsed.split(" ");
+        dateParsed = "" + _.get(months, dateSplit[1]) + "-"
+          + dateSplit[2] + "-" + dateSplit[3];
+        close = quotes[i].close;
+        if (close > max) {
+          max = close;
+        }
+        if (close < max) {
+          min = close;
+        }
+        refined = {
+          x: dateParsed,
+          y: close
+        };
+        quotes[i] = refined;
+
+        // x = _.concat(x,dateParsed);
+        // y = _.concat(y,close);
+
+      }
+
 
       res.send({
-        quotePairs:quotes,
         max: max,
         min:min
       });
