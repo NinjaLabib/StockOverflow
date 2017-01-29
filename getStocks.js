7a6e6dca7b0b93edaab70c2f7efc5da08a1ccfc5
Ihtesham Chowdhury <NinjaLabib@gmail.com>
2017-01-29 04:59:34 -0500

    Good night News API still broken

--- getStocks.js
+++ getStocks.js
@@ -4,8 +4,12 @@ var request = require('request');
 var cheerio = require('cheerio');
 var googleFinance = require('google-finance');
 var _ = require('lodash');
-var times = require('async/times');
+var async = require('async');
+var await = require('await')
  app     = express();
+const months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
+ 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
+ 'Nov':11, 'Dec':12};
 var port_number =(process.env.PORT || 8081);
 app.all('*', function(req, res, next) {
      var origin = req.get('origin');
@@ -31,9 +35,9 @@ app.get('/getData', function(req, res){
     to: toR //req.to
   }, function (err, quotes) {
       console.log(""+quotes[2].date);
-      months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
-      'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
-      'Nov':11, 'Dec':12};
+      // months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
+      // 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
+      // 'Nov':11, 'Dec':12};
       x = [];
       y = [];
       max = -1;
@@ -87,9 +91,7 @@ app.get('/getBounds', function(req, res){
     to: toR //req.to
   }, function (err, quotes) {
       console.log(""+quotes[2].date);
-      months = {'Jan':1, 'Feb':2, 'Mar': 3, 'Apr': 4,
-      'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9,'Oct':10,
-      'Nov':11, 'Dec':12};
+
       x = [];
       y = [];
       max = -1;
@@ -130,41 +132,141 @@ app.get('/getBounds', function(req, res){
   });
 
 })
+function filterNews(options, callback) {
+  request(options,
+    function(err, res3, body) {
+      clean = JSON.parse(body);
+      callback(err, JSON.parse(body));
+    }
+  );
+}
 
-// app.get('/getNews', function(req, res){
-//   var filterNews = function(start, final, q, callback) {
-//     startYear = start.substring(0,4);
-//     startMonth = start.substring(4,6);
-//     endYear = start.substring(0,4);
-//     endMonth = start.substring(4,6);
-//
-//
+function numDays(month, year){
+  if (month == 0 || month == 3 || month == 5 || month == 7 || month == 8 ||
+  month == 10 || month == 12) {
+    return 31;
+  }
+  if(month == 2){
+    if(year%4 == 0){
+      return 29;
+    }
+    return 28;
+  }
+  return 30;
+}
+
+app.get('/getNews', function(req, res){
+  startYear = parseInt(req.query.start.substring(0,4));
+  startMonth = parseInt(req.query.start.substring(4,6));
+  endYear = parseInt(req.query.end.substring(0,4));
+  endMonth = parseInt(req.query.end.substring(4,6));
+  monthsNum = (endYear - startYear)*12 + endMonth - startMonth;
+  company = req.query.company;
+
+  options = [];
+  for(n = 0; n < monthsNum; n++){
+    currentMonth = startMonth + n;
+    currentYear = startYear;
+    if(currentMonth > 12) {
+      addedYears = currentMonth % 12;
+      currentYear = currentYear + addedYears;
+      currentMonth = currentMonth - 12*addedYears;
+    }
+    if(currentMonth < 10){
+      currentMonth = "0"+currentMonth;
+    }
+    begin =  parseInt(""+currentYear+""+currentMonth+"01");
+    end = parseInt(""+currentYear+""+currentMonth+""+numDays(currentMonth,currentYear));
+
+    req = {
+        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+        qs: {
+          'api-key': "b572113326ca44698852b86b199fe673",
+          'q': company,
+          'begin_date':begin,
+          'end_date': end,
+          'page':1,
+          'fq': "document_type:article",
+          'facet_filter': "true"
+        }
+      }
+      // req = {
+      //     url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+      //     qs: {
+      //       'api-key': "b572113326ca44698852b86b199fe673",
+      //       'q': 'GOOGLE',
+      //       'begin_date': "20150201",
+      //       'end_date': "20150401",
+      //       'page':1,
+      //       'facet_filter': "true"
+      //
+      //     }
+      //   }
+    options = _.concat(options,req);
+
+
+  }
+  async.map(options, filterNews, function (err, results){
+    if (err) return console.log(err);
+    for(j = 0; j < results.length;j++){
+      clean = results[j];
+
+      clean = clean.response;
+      if(clean){
+        clean = clean.docs;
+        clean = clean[0];
+        dateUnparsed = ""+clean.pub_date;
+        dateSplit = dateUnparsed.substring(0,10);
+        clean = {
+          url: clean.web_url,
+          snip: clean.snippet,
+          lead_para: clean.lead_paragraph,
+          date: dateSplit
+        }
+        console.log(clean);
+        results[j] = clean;
+      }
+    }
+    res.send(results);
+  });
+
+//   var filterNews = function(n, callback) {
+//     currentMonth = startMonth + n;
+//     currentYear = startYear;
+//     if(currentMonth > 12) {
+//       addedYears = currentMonth % 12;
+//       currentYear = currentYear + addedYears;
+//       currentMonth = currentMonth - 12*addedYears;
+//     }
+//     await(request.get({
+//     url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+//     qs: {
+//       'api-key': "b572113326ca44698852b86b199fe673",
+//       'q': company,
+//       'begin_date': ""+currentYear+""+currentMonth+"01",
+//       'end_date': ""+currentYear+""+currentMonth+"31"
+//     },
+//   }), function(err, response, body) {
+//     body = JSON.parse(body);
 //     callback(null,{
-//       url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-//       qs: {
-//         'api-key': "b572113326ca44698852b86b199fe673",
-//         'q': q,
-//         'begin_date': begin,
-//         'end_date': end
+//       filtered: body.response,
+//       called: currentMonth
 //     })
-//   }
-//   request.get({
-//   url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-//   qs: {
-//     'api-key': "b572113326ca44698852b86b199fe673",
-//     'q': "GOOGLE",
-//     'begin_date': "20140223",
-//     'end_date': "20160312"
-//   },
-// }, function(err, response, body) {
-//   body = JSON.parse(body);
-//   res.send(body);
-// })
+//   })
+//
+// };
+//
+// async.times(months, function(n, next) {
+//     await(filterNews(n, function(err, news) {
+//         next(err, news);
+//     }));
+// }, function(err, filteredNews) {
+//     res.send(filteredNews);
+// });
 //
 //
-//
-//
-// })
+
+})
 
 
 app.listen(port_number);
